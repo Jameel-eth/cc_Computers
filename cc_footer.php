fbc71f779aad40bb84586d86aa2ae01fc55258de
Jameel-eth <126181916+Jameel-eth@users.noreply.github.com>
2023-08-21 16:15:35 -0400

    Initial Upload

---
+++ cc_footer.php
@@ -0,0 +1,10 @@
+<!-- This is the fooer component of the CC Computers project -->
+
+<hr/>
+<table width="100%" style="border: 0;">
+	<tr>
+		<td><strong>Updated:</strong>August 10th 2023</td><td style="text-align: right ;"><strong>Copyright</strong> Coast City Computers</td>
+	</tr>
+	
+
+</table>
